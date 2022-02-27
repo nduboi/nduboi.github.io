8e74c1a176a8216979cbaf6e2443a4f4ba4cc9b9
Nduboi <noa.roussiere@gmail.com>
2022-02-27 15:19:28 +0100

    commit 'main'

---
+++ calculatrice/build/js/main.js
@@ -0,0 +1,88 @@
+var nombre = 0;
+var calcule = 0;
+
+function one() {
+    // var elem = document.getElementById('one'); afficher la ligne html
+    console.log("Vous avez entré 1");
+    nombre =  nombre + " + 1";
+    calcule = calcule  + 1;
+    console.log("calcul", calcule);
+    console.log("adition", nombre);
+}
+function two() {
+    // var elem = document.getElementById('two'); afficher la ligne html
+    console.log("Vous avez entré 2");
+    nombre =  nombre + " + 2";
+    calcule = calcule  + 2;
+    console.log("adition", nombre);
+    console.log("resultat", calcule);
+}
+function three() {
+    // var elem = document.getElementById('two'); afficher la ligne html
+    console.log("Vous avez entré 3");
+    nombre =  nombre + " + 3";
+    calcule = calcule  + 3;
+    console.log("adition", nombre);
+    console.log("resultat", calcule);
+}
+function four() {
+    // var elem = document.getElementById('two'); afficher la ligne html
+    console.log("Vous avez entré 4");
+    nombre =  nombre + " + 4";
+    calcule = calcule  + 4;
+    console.log("adition", nombre);
+    console.log("resultat", calcule);
+}
+function five() {
+    // var elem = document.getElementById('two'); afficher la ligne html
+    console.log("Vous avez entré 5");
+    nombre =  nombre + " + 5";
+    calcule = calcule  + 5;
+    console.log("adition", nombre);
+    console.log("resultat", calcule);
+}
+function six() {
+    // var elem = document.getElementById('two'); afficher la ligne html
+    console.log("Vous avez entré 6");
+    nombre =  nombre + " + 6";
+    calcule = calcule  + 6;
+    console.log("adition", nombre);
+    console.log("resultat", calcule);
+}
+function seven() {
+    // var elem = document.getElementById('two'); afficher la ligne html
+    console.log("Vous avez entré 7");
+    nombre =  nombre + " + 7";
+    calcule = calcule  + 7;
+    console.log("adition", nombre);
+    console.log("resultat", calcule);
+}
+function eight() {
+    // var elem = document.getElementById('two'); afficher la ligne html
+    console.log("Vous avez entré 8");
+    nombre =  nombre + " + 8";
+    calcule = calcule  + 8;
+    console.log("adition", nombre);
+    console.log("resultat", calcule);
+}
+function nine() {
+    // var elem = document.getElementById('two'); afficher la ligne html
+    console.log("Vous avez entré 9");
+    nombre =  nombre + " + 9";
+    calcule = calcule  + 9;
+    console.log("adition", nombre);
+    console.log("resultat", calcule);
+}
+function zero() {
+    // var elem = document.getElementById('two'); afficher la ligne html
+    console.log("Vous avez entré 9");
+    nombre =  nombre + " * 10 ";
+    calcule = calcule  * 10;
+    console.log("adition", nombre);
+    console.log("resultat", calcule);
+}
+function equal(){
+    var affichage = document.getElementById('affichage');
+    affichage.innerHTML = nombre + " = " + calcule;
+
+}
